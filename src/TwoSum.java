19fa54b8804496a17aad70d5494fbf17e9c3d4c9
fjianc <fjianc@gmail.com>
2018-04-13 11:12:06 +0800

    添加少部分注释

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -2,14 +2,29 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * @author FJianC
+ */
 public class TwoSum {
     public static void main(String[] args) {
         int[] nums = {3, 2, 4};
         int target = 6;
-        int[] a = twoSum2(nums, target);
-        System.out.println("[ "+ a[0] + " , " + a[1] + " ]");
+        int[] result;
+        result = twoSum1(nums, target);
+        System.out.println("[ "+ result[0] + " , " + result[1] + " ]");
+        result = twoSum2(nums, target);
+        System.out.println("[ "+ result[0] + " , " + result[1] + " ]");
+        result = twoSum3(nums, target);
+        System.out.println("[ "+ result[0] + " , " + result[1] + " ]");
+        result = twoSum4(nums, target);
+        System.out.println("[ "+ result[0] + " , " + result[1] + " ]");
+        result = twoSum5(nums, target);
+        System.out.println("[ "+ result[0] + " , " + result[1] + " ]");
     }
-    public static int[] twoSum1(int[] nums, int target) {
+    /**
+     * 这个算法很骚。。
+     * */
+    private static int[] twoSum1(int[] nums, int target) {
         int numMin = Integer.MAX_VALUE;
         int numMax = Integer.MIN_VALUE;
         for (int num : nums) {
@@ -40,8 +55,10 @@ public class TwoSum {
         }
         return new int[] { 0, 0 };
     }
-
-    public static int[] twoSum2(int[] nums, int target) {
+    /**
+     * Arrays.sort()，底层是快排，O(nlogn)
+     * */
+    private static int[] twoSum2(int[] nums, int target) {
         int[] result = new int[2];
         int[] sortedArray = Arrays.copyOf(nums, nums.length);
         Arrays.sort(sortedArray);
@@ -69,28 +86,29 @@ public class TwoSum {
         }
         return result;
     }
-
-    public static int[] twoSum3(int[] nums, int target) {
+    /**
+     * 快排，O(nlogn)
+     * */
+    private static int[] twoSum3(int[] nums, int target) {
         int length = nums.length;
-        if(length<20){
-            for(int i=length-1; i>0; i--){
-                for(int j=0; j<i; j++){
-                    if(nums[j] + nums[i] == target){
-                        int[] ret = {j,i};
-                        return ret;
+        if (length < 20) {
+            for (int i = length - 1; i > 0; i--) {
+                for (int j = 0; j < i; j++) {
+                    if (nums[j] + nums[i] == target) {
+                        return new int[]{j, i};
                     }
                 }
             }
         }
         else{
             int[] sortIndex = new int[length];
-            for(int i=0; i<length; i++){
+            for (int i = 0; i < length; i++) {
                 sortIndex[i] = i;
             }
-            mergeSort(nums, sortIndex, 0, length-1);
+            mergeSort(nums, sortIndex, 0, length - 1);
             int left = 0;
-            int right = nums.length-1;
-            while(left<right){
+            int right = nums.length - 1;
+            while (left < right) {
                 int flag = nums[sortIndex[left]] + nums[sortIndex[right]] - target;
                 if(flag == 0){
                     break;
@@ -102,22 +120,22 @@ public class TwoSum {
                     left++;
                 }
             }
-            int[] ret = {sortIndex[left],sortIndex[right]};
-            return ret;
+
+            return new int[]{sortIndex[left], sortIndex[right]};
         }
-        return null;
+        return new int[]{};
     }
-    public static void mergeSort(int[] nums, int[] index, int begin, int end){
+    private static void mergeSort(int[] nums, int[] index, int begin, int end){
         if(begin < end){
-            int mid = (begin+end)/2;
-            mergeSort(nums,index,begin,mid);
-            mergeSort(nums,index,mid+1,end);
-            int i=begin,j=mid+1,k=0;
-            int[] mergeIndex = new int[end-begin+1];
-            while(i<=mid && j<=end){
+            int mid = (begin + end) / 2;
+            mergeSort(nums, index, begin, mid);
+            mergeSort(nums, index, mid + 1, end);
+            int i = begin,j = mid + 1,k = 0;
+            int[] mergeIndex = new int[end - begin + 1];
+            while (i <= mid && j <= end) {
                 int left = nums[index[i]];
                 int right = nums[index[j]];
-                if(left<right){
+                if(left < right){
                     mergeIndex[k] = index[i];
                     i++;
                 }
@@ -127,19 +145,20 @@ public class TwoSum {
                 }
                 k++;
             }
-            if(i>mid){
-                System.arraycopy(index, j, mergeIndex, k, end-j+1);
+            if (i > mid) {
+                System.arraycopy(index, j, mergeIndex, k, end - j + 1);
             }
-            if(j>end){
-                System.arraycopy(index, i, mergeIndex, k, mid-i+1);
+            if (j > end) {
+                System.arraycopy(index, i, mergeIndex, k, mid - i + 1);
             }
-            System.arraycopy(mergeIndex, 0, index, begin, end-begin+1);
+            System.arraycopy(mergeIndex, 0, index, begin, end - begin + 1);
         }
     }
-
-    public static int[] twoSum4(int[] nums, int target) {
-        // key为值，value为索引
-        Map<Integer, Integer> map = new HashMap<>();
+    /**
+     * HashMap，利用哈希算法，O(n)
+     * */
+    private static int[] twoSum4(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
         for (int i = 0; i < nums.length; i++) {
             Integer res = map.get(target - nums[i]);
             if (res != null) {
@@ -151,18 +170,21 @@ public class TwoSum {
         }
         return new int[]{};
     }
-
-    public static int[] twoSum5(int[] nums, int target) {
+    /**
+     * 蛮力法 O(n2)
+     * */
+    private static int[] twoSum5(int[] nums, int target) {
         if (nums.length == 0 || nums.length == 1) {
             return  new int[]{-1};
         }
-        for (int i = 0;i < nums.length;i ++) {
-            for (int j = 0;j !=i && j< nums.length;j ++) {
+        int i, j;
+        for (i = 0;i < nums.length; i++) {
+            for (j = i + 1; j < nums.length; j++) {
                 if ((nums[i] + nums[j]) == target) {
                     return new int[]{i, j};
                 }
             }
         }
-        return new int[]{-1};
+        return new int[]{};
     }
 }
